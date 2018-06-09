91d9189a1a880d7ace3e91b1dd91cf330de878da
付文彩 <328974233@qq.com>
2018-06-09 16:57:44 +0800

    修改ChargeRules字段

--- Python_tensorflow_LicensePlate/entity/ChargeRules.py
+++ Python_tensorflow_LicensePlate/entity/ChargeRules.py
@@ -1,6 +1,12 @@
 class ChargeRules:
     '收费规则'
-    def __init__(self,price,begintime,endtime):
-        self.price = price
-        self.begintime = begintime
-        self.endtime = endtime
+    def __init__(self,rid,dayprice,nightprice,daybegintime,dayendtime,
+                 nightbegintime,nightendtime,firsthourprice):
+        self.rid = rid
+        self.dayprice = dayprice
+        self.nightprice = nightprice
+        self.daybegintime = daybegintime
+        self.dayendtime = dayendtime
+        self.nightbegintime = nightbegintime
+        self.nightendtime = nightendtime
+        self.firsthourprice = firsthourprice
