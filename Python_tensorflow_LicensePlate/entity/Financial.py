fdc6ffc74c869ab90a687120fb25cbeeecb679cf
付文彩 <328974233@qq.com>
2018-06-09 17:43:28 +0800

    增加financial类

---
+++ Python_tensorflow_LicensePlate/entity/Financial.py
@@ -0,0 +1,8 @@
+class Financial():
+    '财务报表类'
+    def __init__(self, Fid,ParkPlaceID, chargetime, money):
+        self.ParkPlaceID = ParkPlaceID
+        self.chargetime = chargetime
+        self.money = money
+        self.Fid = None
+
