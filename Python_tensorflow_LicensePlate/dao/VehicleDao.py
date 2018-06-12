3341a1318582a0357509ae7bee4810970cc9e3fb
王燕 <1490315929@qq.com>
2018-06-12 15:11:35 +0800

    车辆信息管理方法参数的修改

--- Python_tensorflow_LicensePlate/dao/VehicleDao.py
+++ Python_tensorflow_LicensePlate/dao/VehicleDao.py
@@ -1,4 +1,3 @@
-from  Python_tensorflow_LicensePlate.entity.Vehicle import Vehicle
 from abc import ABCMeta, abstractmethod
 #抽象类
 class VehicleDao(object):
@@ -7,26 +6,32 @@ class VehicleDao(object):
     def __init__(self):
         self.VehicleDao = ''
 
+    #添加车辆信息
     @abstractmethod
-    def addVehicle(self):
+    def addVehicle(self,Vehicle):
         pass
 
+    #修改车辆信息
     @abstractmethod
     def updateVehicle(self,SID):
         pass
 
+    #通过员工号查找车辆信息
     @abstractmethod
     def findVehicleBySID(self,SID):
         pass
 
+    #通过车牌号查找车辆信息
     @abstractmethod
     def findVehicleByPlateID(self,PlateID):
         pass
 
+    #显示所有车辆信息
     @abstractmethod
     def showallVehicle(self):
         pass
 
+    #删除车辆信息
     @abstractmethod
     def deleteVehicle(self,SID):
         pass
