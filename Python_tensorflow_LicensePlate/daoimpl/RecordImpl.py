9065469032e576df054055b2d8ac61a57c3be98c
王燕 <1490315929@qq.com>
2018-06-14 14:16:57 +0800

    记录的数据查询操作实现

--- Python_tensorflow_LicensePlate/daoimpl/RecordImpl.py
+++ Python_tensorflow_LicensePlate/daoimpl/RecordImpl.py
@@ -1,5 +1,6 @@
 from Python_tensorflow_LicensePlate.dao import RecordDao
 from Python_tensorflow_LicensePlate.utils import Pymysql
+from Python_tensorflow_LicensePlate.entity.Record import Record
 
 class RecordImpl(RecordDao):
 
@@ -24,3 +25,70 @@ class RecordImpl(RecordDao):
     def deleteRecordByPlateNumber(self, platenumber):
         sql = "delete from record where platenumber = {0}".format(platenumber)
         Pymysql.PyMySQLHelper.update(sql)
+
+    #根据车牌号查找车辆记录
+    def findRecordByPlateID(self, platenumber):
+        py = Pymysql.PyMySQLHelper
+        sql = 'select * from record where PNumber = {0}'.format(platenumber)
+        result = py.selectalldictcursor(sql)
+        list = []
+        for rs in result:
+            PNumber = rs['PNumber']
+            intime = rs['intime']
+            outtime = rs['outtime']
+            vehicletype = rs['vehicletype']
+            feestatus = rs['feestatus']
+            record = Record(PNumber, intime, outtime, vehicletype, feestatus)
+            list.append(record)
+        return list
+
+    # 根据车辆离开时间按年查找车辆记录
+    def findRecordByYear(self, year):
+        py = Pymysql.PyMySQLHelper
+        sql = 'select * from record where year(outtime) = {0}'.format(year)
+        result = py.selectalldictcursor(sql)
+        list = []
+        for rs in result:
+            PNumber = rs['PNumber']
+            intime = rs['intime']
+            outtime = rs['outtime']
+            vehicletype = rs['vehicletype']
+            feestatus = rs['feestatus']
+            record = Record(PNumber, intime, outtime, vehicletype, feestatus)
+            list.append(record)
+        return list
+
+    # 根据车辆离开时间按月查找车辆记录
+    def findRecordByMonth(self, month):
+        py = Pymysql.PyMySQLHelper
+        sql = 'select * from record where month(outtime) = {0}'.format(month)
+        result = py.selectalldictcursor(sql)
+        list = []
+        for rs in result:
+            PNumber = rs['PNumber']
+            intime = rs['intime']
+            outtime = rs['outtime']
+            vehicletype = rs['vehicletype']
+            feestatus = rs['feestatus']
+            record = Record(PNumber, intime, outtime, vehicletype, feestatus)
+            list.append(record)
+        return list
+
+    # 根据车辆离开时间按日查找车辆记录
+    def findRecordByDay(self, day):
+        py = Pymysql.PyMySQLHelper
+        sql = 'select * from record where day(outtime) = {0}'.format(day)
+        result = py.selectalldictcursor(sql)
+        list = []
+        for rs in result:
+            PNumber = rs['PNumber']
+            intime = rs['intime']
+            outtime = rs['outtime']
+            vehicletype = rs['vehicletype']
+            feestatus = rs['feestatus']
+            record = Record(PNumber, intime, outtime, vehicletype, feestatus)
+            list.append(record)
+        return list
+
+
+
