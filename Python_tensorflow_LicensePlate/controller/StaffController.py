8db3e56abd2c8d5ee73a0ffb697d2610540b4410
王燕 <1490315929@qq.com>
2018-06-20 21:38:24 +0800

    员工信息管理控制层

--- Python_tensorflow_LicensePlate/controller/StaffController.py
+++ Python_tensorflow_LicensePlate/controller/StaffController.py
@@ -15,6 +15,34 @@ class StaffController:
         result = s.addStaff(staff)
         return result
 
+    def updStaff(self, StaffNum, carNum, name, phone, gender, department):
+        staff = Staff()
+        staff.setDepartment(department)
+        staff.setGender(gender)
+        staff.setName(name)
+        staff.setPhoneNumber(phone)
+        staff.setSID(StaffNum)
+        staff.setVehicleQuantity(carNum)
+        s = StaffService()
+        result = s.updateStaff(staff)
+        return result
+
     def showStaff(self):
         s = StaffService()
         return s.showStaff()
+
+    def delStaff(self,sid):
+        s = StaffService()
+        return s.deleteStaff(sid)
+
+    def findStaffByid(self,sid):
+        s = StaffService()
+        return s.findStaffById(sid)
+
+    def findStaffByname(self,sname):
+        s = StaffService()
+        return s.findStaffByName(sname)
+
+
+
+
