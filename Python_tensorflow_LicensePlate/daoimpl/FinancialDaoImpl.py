220e89796e89b6222e2f4c59980ea51e1ebf4a5b
付文彩 <328974233@qq.com>
2018-06-20 20:33:53 +0800

    sql字符串format格式化改为%s格式化形式，避免所赋值内容为字符串格式时出错

--- Python_tensorflow_LicensePlate/daoimpl/FinancialDaoImpl.py
+++ Python_tensorflow_LicensePlate/daoimpl/FinancialDaoImpl.py
@@ -6,23 +6,25 @@ from Python_tensorflow_LicensePlate.entity.Financial import Financial
 class FinancialDaoImpl(FinancialDao):
     def insertfinancial(self, financial):
         """插入一个financial对象"""
-        sql = 'insert into financial (ParkPlaceID, time, money) VALUES ({0},{1},{2})'.format(
-            financial.ParkPlaceID,financial.time,financial.money)
-        count = PyMySQLHelper().update(sql)
+        sql = 'insert into financial (ParkPlaceID, chargetime, money) VALUES (%s,%s,%s)'
+        params =(financial.ParkPlaceID,financial.chargetime,financial.money)
+        print(sql)
+        count = PyMySQLHelper().updateByParam(sql,params)
         return count
 
     def deletefinancial(self, financialid):
         """根据id删除"""
-        sql = 'delete from financial WHERE financialid = {0}'.format(financialid)
-        count = PyMySQLHelper().update(sql)
+        sql = 'delete from financial WHERE Fid = %s'
+        parms =(financialid)
+        count = PyMySQLHelper().updateByParam(sql,parms)
         return  count
 
     def updatefinancial(self, financial):
         "更新"
-        sql = 'update financial set (ParkPlaceID,' \
-              'chargetime,money) VALUES ({0},{1},{2})' \
-              ' WHERE Fid = {3}'.format(financial.ParkPlaceID,financial.chargetime,financial.money)
-        count = PyMySQLHelper().update(sql)
+        sql = ('update financial set ParkPlaceID = %s,' 
+              'chargetime = %s,money = %s  WHERE Fid = %s')
+        parms = (financial.ParkPlaceID,financial.chargetime,financial.money,financial.Fid)
+        count = PyMySQLHelper().updateByParam(sql,parms)
         return count
 
     def listfinancialbyparkplaceid(self, parkplaceid):
@@ -41,7 +43,8 @@ class FinancialDaoImpl(FinancialDao):
 
 
     def listfinancialbyyear(self, year):
-        """"返回某一年的财务列表"""
+        """"返回某一年的财务列表
+        参数为格式类型为 '2018'"""
         sql = 'select * from financial WHERE year(chargetime) = {0}'.format(year)
         result = PyMySQLHelper().selectalldictcursor(sql)
         list = []
@@ -55,9 +58,12 @@ class FinancialDaoImpl(FinancialDao):
         return list
 
     def listfinancialbymonth(self, month):
-        """返回某一月的财务列表"""
-        sql = 'select * from financial WHERE month(chargetime) = {0}'.format(month)
-        result = PyMySQLHelper().selectalldictcursor(sql)
+        """返回某一月的财务列表
+        参数格式为'2018-06'
+        """
+        sql = "select * from financial WHERE DATE_FORMAT(chargetime,'%%Y-%%m')= %s"
+        params = (month)
+        result = PyMySQLHelper().selectalldictcursorByparams(sql,params)
         list = []
         for rs in result:
             Fid = rs['Fid']
@@ -69,9 +75,12 @@ class FinancialDaoImpl(FinancialDao):
         return list
 
     def listfinancialbyday(self, day):
-        """返回某一天的财务列表"""
-        sql = 'select * from financial WHERE day(chargetime) = {0}'.format(day)
-        result = PyMySQLHelper().selectalldictcursor(sql)
+        """返回某一天的财务列表
+        参数格式为'2018-6-20'"""
+        sql ="select * from financial WHERE DATE_FORMAT(chargetime,'%%Y-%%m-%%d') = %s"
+        params = (day)
+        result = PyMySQLHelper().selectalldictcursorByparams(sql,params)
+        print(result)
         list = []
         for rs in result:
             Fid = rs['Fid']
