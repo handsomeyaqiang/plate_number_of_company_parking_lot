9fd0c3a72584a94d9e94ad99c1dedabaf22ac619
付文彩 <328974233@qq.com>
2018-06-20 23:50:31 +0800

    修改参数

--- Python_tensorflow_LicensePlate/entity/ParkPlace.py
+++ Python_tensorflow_LicensePlate/entity/ParkPlace.py
@@ -1,10 +1,10 @@
 class ParkPlace(object):
     '停车位类'
 
-    def __init__(self,  lockStatus, parkPlaceType):
+    def __init__(self,  lockStatus, parkPlaceType,useCarNumber):
         self.parkPlaceID = None
         self.lockStatus = lockStatus
         self.parkPlaceType = parkPlaceType
-        self.useCarNumber = None
+        self.useCarNumber = useCarNumber
 
 
