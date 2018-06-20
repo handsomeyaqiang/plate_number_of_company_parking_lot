0164d8da2878af8add564d0f7392fc9c02aa3494
王燕 <1490315929@qq.com>
2018-06-20 17:52:48 +0800

    测试后更正访问数据库方法

--- Python_tensorflow_LicensePlate/daoimpl/VehicleDaoImpl.py
+++ Python_tensorflow_LicensePlate/daoimpl/VehicleDaoImpl.py
@@ -1,38 +1,50 @@
 from Python_tensorflow_LicensePlate.entity.Vehicle import Vehicle
 from Python_tensorflow_LicensePlate.dao.VehicleDao import VehicleDao
-from ..utils import Pymysql
+from Python_tensorflow_LicensePlate.utils import Pymysql
 #实现类
 class VehilceDaoImpl(VehicleDao):
     def __init__(self):
         self.VehicleDaoImpl = ''
 
     #添加车辆信息实现
-    def addVehicle(self,Vehicle):
-        py =Pymysql.PyMySQLHelper
-        sql = 'insert into vehicle(PlateID,owner,vehicle_identity,SID) VALUES ({0},{1},{2},{3})'.format(
-            Vehicle.PlateID,Vehicle.owner,Vehicle.vehicle_identity,Vehicle.SID)
-        count = py.update(sql)
+    def addVehicle(self,vehicle):
+        py =Pymysql.PyMySQLHelper()
+        sql = 'insert into vehicle(PlateID,owner,Vehicle_identity,SID) ' \
+              'VALUES (%s,%s,%s,%s)'
+        params = (vehicle.PlateID,vehicle.owner,vehicle.vehicle_identity,vehicle.SID)
+        count = py.updateByParam(sql, params)
         return count
 
-    # 修改车辆信息
-    def updateVehicle(self,Vehicle):
-        py = Pymysql.PyMySQLHelper
-        sql = 'update vehicle set (PlateID,owner,vehicle_identity) VALUES ({0},{1},{2})' \
-              ' WHERE SID = {3}'.format(Vehicle.PlateID,Vehicle.owner,Vehicle.vehicle_identity)
-        count = py.update(sql)
+    # 修改车牌号修改车辆信息
+    def updateVehicle(self,vehicle):
+        py = Pymysql.PyMySQLHelper()
+        sql = 'update vehicle set owner=%s,Vehicle_identity=%s,SID=%s where PlateID=%s'
+        params = ( vehicle.owner, vehicle.vehicle_identity, vehicle.SID,vehicle.PlateID)
+        count = py.updateByParam(sql, params)
         return count
 
-    # 删除车辆信息
-    def deleteVehicle(self,SID):
-        py = Pymysql.PyMySQLHelper
-        sql = 'delete from vehicle WHERE SID = {0}'.format(SID)
-        count = py.update(sql)
+    # 根据员工号删除车辆信息
+    def deleteVehicleBySID(self,SID):
+        py = Pymysql.PyMySQLHelper()
+        sql = 'delete from vehicle WHERE SID =%s'
+        params=(SID)
+        count = py.updateByParam(sql, params)
         return count
+
+
+    # 根据车牌号删除车辆信息
+    def deleteVehicleByPlateID(self,PlateID):
+        py = Pymysql.PyMySQLHelper()
+        sql = 'delete from vehicle WHERE PlateID =%s'
+        params=(PlateID)
+        count = py.updateByParam(sql, params)
+        return count
+
 
     # 通过员工号查找车辆信息实现
     def findVehicleBySID(self, SID):
-        py = Pymysql.PyMySQLHelper
-        sql = 'select * from vehicle where SID = {0}'.format(SID)
+        py = Pymysql.PyMySQLHelper()
+        sql = "select * from vehicle where SID ='%s'"%(SID)
         result = py.selectalldictcursor(sql)
         list = []
         for rs in result:
@@ -40,28 +52,30 @@ class VehilceDaoImpl(VehicleDao):
             PlateID=['PlateID']
             owner=['owner']
             vehicle_identity=['vehicle_identity']
-            vehicle = Vehicle(SID, PlateID,owner,vehicle_identity)
+            vehicle = Vehicle(SID,PlateID, owner, vehicle_identity)
             list.append(vehicle)
         return list
 
     # 通过车牌号查找车辆信息实现
     def findVehicleByPlateID(self, PlateID):
-        py = Pymysql.PyMySQLHelper
+        py = Pymysql.PyMySQLHelper()
         sql = "select * from vehicle where PlateID = '%s'"%(PlateID)
-        result = py.selectalldictcursor(sql)
+        result=py.selectOne(sql)
         list = []
         for rs in result:
             SID = rs['SID']
-            PlateID=['PlateID']
-            owner=['owner']
-            vehicle_identity=['vehicle_identity']
-            vehicle = Vehicle(SID, PlateID,owner,vehicle_identity)
+            PlateID = ['PlateID']
+            owner = ['owner']
+            vehicle_identity = ['vehicle_identity']
+            vehicle = Vehicle(SID,PlateID, owner, vehicle_identity)
             list.append(vehicle)
         return list
 
+
+
     # 显示所有车辆信息实现
     def showallVehicle(self):
-        py = Pymysql.PyMySQLHelper
+        py = Pymysql.PyMySQLHelper()
         sql = 'select * from vehicle'
         result = py.selectalldictcursor(sql)
         list = []
@@ -70,7 +84,7 @@ class VehilceDaoImpl(VehicleDao):
             PlateID=['PlateID']
             owner=['owner']
             vehicle_identity=['vehicle_identity']
-            vehicle = Vehicle(SID, PlateID,owner,vehicle_identity)
+            vehicle = Vehicle(SID,PlateID,owner,vehicle_identity)
             list.append(vehicle)
         return list
 
