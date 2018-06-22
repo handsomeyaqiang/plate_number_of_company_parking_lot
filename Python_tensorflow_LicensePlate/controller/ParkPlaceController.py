7d300e6132a50b2cd2ee81b50d6391a9f25beb9d
付文彩 <328974233@qq.com>
2018-06-22 15:47:10 +0800

    提交控制层车位分配和车位回收实现

--- Python_tensorflow_LicensePlate/controller/ParkPlaceController.py
+++ Python_tensorflow_LicensePlate/controller/ParkPlaceController.py
@@ -19,11 +19,13 @@ class ParkPlaceController(object):
         """增加一个内部车位"""
         rs = ParkPlaceService().addsingelinnerparkplace()
         return rs
-    def adddulparkplace(self,type,number):
+
+    def adddulparkplace(self, type, number):
         """增加多个车位"""
-        rs = ParkPlaceService().adddulparkplace(type,number)
+        rs = ParkPlaceService().adddulparkplace(type, number)
         return rs
-    def deleteparkplacebyid(self,parkplaceid):
+
+    def deleteparkplacebyid(self, parkplaceid):
         """根据车位号删除车位"""
         rs = ParkPlaceService().deleteparkplacenyid(parkplaceid)
         return rs
@@ -36,7 +38,7 @@ class ParkPlaceController(object):
     def getempty_tempparkplacecount(self):
         """获取空闲临时车位数量"""
         rs = ParkPlaceService().getempty_tempparkplacecount()
-        return  rs
+        return rs
 
     def getinnernumber(self):
         """获取内部车位数量"""
@@ -55,10 +57,10 @@ class ParkPlaceController(object):
 
     def gettempnumber(self):
         """获取临时车位数量"""
-        rs =ParkPlaceService().gettempnumber()
+        rs = ParkPlaceService().gettempnumber()
         return rs
 
-    def lock(self,parkplaceid):
+    def lock(self, parkplaceid):
         """根据传入的车位号进行关锁操作"""
         rs = ParkPlaceService().lock(parkplaceid)
         return rs
@@ -68,16 +70,33 @@ class ParkPlaceController(object):
         rs = ParkPlaceService().showparkplaceinformation()
         return rs
 
-    def unlock(self,parkplaceid):
+    def unlock(self, parkplaceid):
         """根据传入的车位号进行打开锁操作"""
         rs = ParkPlaceService().unlock(parkplaceid)
         return rs
 
-    def updateparkplace(self,parkPlaceID,lockStatus, parkPlaceType,useCarNumber):
+    def updateparkplace(self, parkPlaceID, lockStatus, parkPlaceType, useCarNumber):
         """更新车位信息"""
-        parkplace = ParkPlace(lockStatus, parkPlaceType,useCarNumber)
+        parkplace = ParkPlace(lockStatus, parkPlaceType, useCarNumber)
         parkplace.parkPlaceID = parkPlaceID
         rs = ParkPlaceService().updateparkplace(parkplace)
         return rs
 
+    def allocateparkplace(self, carnumber, type):
+        """
+        进入停车场时根据车辆类型随机分配相应类型的车位
+        :param carnumber: 车牌号
+        :param type: 车辆类型 0 内部车 1 临时车
+        :return: rs中的data属性携带所分配的车位号
+        """
+        rs = ParkPlaceService().allocateparkplace(carnumber, type)
+        return rs
 
+    def reclaimparkpalce(self, parkplaceid):
+        """
+        离开停车场时，根据传入的车位号回收车位
+        :param parkplaceid: 车位号
+        :return:
+        """
+        rs = ParkPlaceService().reclaimparkpalce(parkplaceid)
+        return rs
