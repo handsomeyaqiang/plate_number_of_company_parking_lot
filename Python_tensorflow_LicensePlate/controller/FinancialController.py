5c90f00afd79c7bf8efa1239de43734df506ad42
付文彩 <328974233@qq.com>
2018-06-26 16:41:27 +0800

    财务控制层增加返回某一年，某一月，某一日及返回全部记录的方法，以及返回某一年的每个月的收入和的方法

--- Python_tensorflow_LicensePlate/controller/FinancialController.py
+++ Python_tensorflow_LicensePlate/controller/FinancialController.py
@@ -1,11 +1,62 @@
-from  Python_tensorflow_LicensePlate.entity.Financial import Financial
+from Python_tensorflow_LicensePlate.entity.Financial import Financial
 from Python_tensorflow_LicensePlate.service.FinancialService import FinancialService
 
 
 class FinancialController(object):
     """财务控制类"""
-    def insertFinancial(self,ParkPlaceID, chargetime, money):
+
+    def insertFinancial(self, ParkPlaceID, chargetime, money):
         """插入一条记录"""
         fin = Financial(ParkPlaceID, chargetime, money)
         rs = FinancialService().insertFinancial(fin)
+        return rs
+
+    def listallfinancial(self):
+        """返回所有财务记录"""
+        rs = FinancialService().listallfinancial()
+        return rs
+
+    def listbyday(self, day):
+        """
+        返回某一天的财务记录
+        :param day: 日期 格式为：‘2018-06-05’
+        :return:
+        """
+        rs = FinancialService().listbyday(day)
+        return rs
+
+    def listbymonth(self, month):
+        """
+        返回某个月的所有记录
+        :param month: 月份 格式为‘2018-06’
+        :return:
+        """
+        rs = FinancialService().listbymonth(month)
+        return rs
+
+    def listbyyear(self, year):
+        """
+        返回某一年的全部记录
+        :param year:年份 格式为‘2018’
+        :return:
+        """
+        rs = FinancialService().listbyyear(year)
+        return rs
+
+    def listbyparkolaceid(self,parkplaceid):
+        """
+        返回某个车位的收入记录
+        :param parkplaceid: 车位号
+        :return:
+        """
+        rs = FinancialService().listbyparkplaceid(parkplaceid)
+        return rs
+
+    def listmonthsumbyyear(self,year):
+        """
+        返回某年的每个月份的收入和
+        :param year: 年份格式为"2018"
+        :return:
+        """
+        rs = FinancialService().listmonthsumbyyear(year)
         return rs
